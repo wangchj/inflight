d3b1c85b698c24b8d0c34121c0e1ca0ac7158086
Jeff Wang <1118410+wangchj@users.noreply.github.com>
2024-12-27 23:59:49 -0800

    Remove types/tree-node.ts

--- src/types/tree-node.ts
+++
@@ -1,7 +0,0 @@
-import { TreeNodeData } from "@mantine/core";
-import { Folder } from "types/folder";
-
-export interface TreeNode extends TreeNodeData {
-  type: 'folder' | 'request';
-  parentFolder: Folder;
-}
